e46a375f9fc79d10ea2cd8a40c0bd11930380cae
94ycwang <35606086+94ycwang@users.noreply.github.com>
2018-03-22 17:22:45 -0400

    change library

--- index.js
+++ index.js
@@ -302,4 +302,13 @@ function download() {
 	}
 };	
 //*************************************************** print snapshot***************************************
-L.control.browserPrint().addTo(mymap)
+L.control.browserPrint({
+	printLayer: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+					attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+					subdomains: 'abcd',
+					minZoom: 1,
+					maxZoom: 16,
+					ext: 'png'
+				}),
+	closePopupsOnPrint: true    
+}).addTo(mymap)
