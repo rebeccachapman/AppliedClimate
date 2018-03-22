c4166c2b23a52b6fb3548df7b3882b797d3e5ce6
94ycwang <35606086+94ycwang@users.noreply.github.com>
2018-03-22 17:29:39 -0400

    Add print map

--- index.js
+++ index.js
@@ -302,13 +302,9 @@ function download() {
 	}
 };	
 //*************************************************** print snapshot***************************************
-L.control.browserPrint({
-	printLayer: L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-					attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-					subdomains: 'abcd',
-					minZoom: 1,
-					maxZoom: 16,
-					ext: 'png'
-				}),
-	closePopupsOnPrint: true    
-}).addTo(mymap)
+L.easyPrint({
+	filename:"HOPMmap"	
+	position: 'topleft',
+	sizeModes: ['A4Portrait', 'A4Landscape'],
+	exportOnly: true
+}).addTo(mymap);
